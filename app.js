80e83eab7251420e60a197c28742fa59576a185f
Resek <matija.resek@gmail.com>
2017-10-24 17:47:54 +0200

    new challenge route

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-express = require('express');
-app = express();
+var express = require('express');
+var app = express();
 
 app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 app.get('/', function (req, res) {
-  res.render("index.ejs");
-})
+  res.render("index");
+});
+
+//new challenge
+app.get("/challenges/new", function(req, res) {
+  res.render("challenges/new");
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
