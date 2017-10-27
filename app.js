27313e464c1996a974663810160aaea08cc3df16
Resek <matija.resek@gmail.com>
2017-10-27 17:32:23 +0200

    env port

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.get("/challenges/:id", function (req, res) {
 });
 
 //server
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen (process.env.PORT, process.env.IP, function() {
+  console.log ("BlogApp has started!");
 });
