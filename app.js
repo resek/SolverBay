db9e6029dd05125817b7ec19e2831075273fe10b
Resek <matija.resek@gmail.com>
2017-11-08 15:48:09 +0100

    update / delete solutions routes work

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express      = require('express');
-var bodyParser   = require('body-parser');
-var mongoose     = require('mongoose');
-var seedDB       = require("./seeds");
-var app          = express();
+var express         = require('express');
+var methodOverride  = require('method-override');
+var bodyParser      = require('body-parser');
+var mongoose        = require('mongoose');
+var seedDB          = require("./seeds");
+var app             = express();
 
 var Challenge = require ("./models/challenge");
 var Solution = require ("./models/solution");
 
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -87,6 +89,54 @@ app.post ("/challenges/:id/solutions", function (req, res) {
         } else { 
           foundChallenge.solutions.push(newSolution);
           foundChallenge.save();                
+          res.redirect ("/challenges/" + req.params.id);
+        }
+      });
+    }
+  });
+});
+
+//solutions - edit
+app.get ("/challenges/:id/solutions/:solutionId/edit", function (req, res) {
+  Challenge.findById(req.params.id, function(err, foundChallenge) {
+    if(err) {
+        console.log(err);
+    } else {
+      Solution.findById(req.params.solutionId, function(err, foundSolution) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render ("solution/edit", {foundChallenge: foundChallenge, foundSolution: foundSolution});
+        }
+      });
+    }
+  });
+});
+
+//solutions - update
+app.put ("/challenges/:id/solutions/:solutionId", function (req, res) {
+  console.log(req.body);
+  Solution.findByIdAndUpdate (req.params.solutionId, req.body, function (err) {
+    if(err) {
+        console.log(err); 
+    } else {
+      res.redirect ("/challenges/" + req.params.id);
+    }
+  });
+});
+
+//solutions - delete
+app.delete ("/challenges/:id/solutions/:solutionId", function(req, res) {
+  Solution.findByIdAndRemove (req.params.solutionId, function (err) {
+    if(err) {
+      console.log(err);
+    } else {
+      Challenge.findById(req.params.id, function (err, foundChallenge) {
+        if (err) {
+            console.log(err);
+        } else {
+          foundChallenge.solutions.remove(req.params.solutionId); 
+          foundChallenge.save();
           res.redirect ("/challenges/" + req.params.id);
         }
       });
