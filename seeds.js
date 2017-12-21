28bbd8c447828b074c7bc92399d3dab5738e3360
Resek <matija.resek@gmail.com>
2017-12-21 11:49:02 +0100

    € / $ frontend

--- seeds.js
+++ seeds.js
@@ -8,6 +8,7 @@ var testChallenges = [
         title: "Decrease air inlet when domestic refrigerator door is opened",
         description: "When we open the refrigerator door the cold air flows out and is replaced by the warm and moist one. This presents a problem for the cooling system. We are looking for ideas how to reduce it. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         prize: 50,
+        currency: "USD",
         date: '2017-12-16',
         author: {username: "donsan", id: "5b0d6cdcb6d39e055e418882"},
         isPaid: true 
@@ -16,6 +17,7 @@ var testChallenges = [
         title: "Decrease air inlet when domestic refrigerator door is opened",
         description: "When we open the refrigerator door the cold air flows out and is replaced by the warm and moist one. This presents a problem for the cooling system. We are looking for ideas how to reduce it. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         prize: 50,
+        currency: "EUR",
         date: '2017-12-22',
         author: {username: "donsan", id: "5b0d6cdcb6d39e055e418882"},
         isPaid: false
@@ -24,6 +26,7 @@ var testChallenges = [
         title: "Decrease air inlet when domestic refrigerator door is opened",
         description: "When we open the refrigerator door the cold air flows out and is replaced by the warm and moist one. This presents a problem for the cooling system. We are looking for ideas how to reduce it. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         prize: 50,
+        currency: "EUR",
         date: '2017-12-25',
         author: {username: "donsan", id: "5b0d6cdcb6d39e055e418882"},
         isPaid: true
@@ -32,6 +35,7 @@ var testChallenges = [
         title: "Decrease air inlet when domestic refrigerator door is opened",
         description: "When we open the refrigerator door the cold air flows out and is replaced by the warm and moist one. This presents a problem for the cooling system. We are looking for ideas how to reduce it. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         prize: 50,
+        currency: "USD",
         date: '2017-12-19',
         author: {username: "donsan", id: "5b0d6cdcb6d39e055e418882"},
         isPaid: true
@@ -40,6 +44,7 @@ var testChallenges = [
         title: "Decrease air inlet when domestic refrigerator door is opened",
         description: "When we open the refrigerator door the cold air flows out and is replaced by the warm and moist one. This presents a problem for the cooling system. We are looking for ideas how to reduce it. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         prize: 50,
+        currency: "USD",
         date: '2017-12-27',
         author: {username: "donsan", id: "5b0d6cdcb6d39e055e418882"},
         isPaid: true
@@ -48,6 +53,7 @@ var testChallenges = [
         title: "Decrease air inlet when domestic refrigerator door is opened",
         description: "When we open the refrigerator door the cold air flows out and is replaced by the warm and moist one. This presents a problem for the cooling system. We are looking for ideas how to reduce it. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         prize: 50,
+        currency: "EUR",
         date: '2017-12-23',
         author: {username: "donsan", id: "5b0d6cdcb6d39e055e418882"},
         isPaid: true
